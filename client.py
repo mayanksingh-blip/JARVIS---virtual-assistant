b8cf806793eacd6e83e3e102fa0c8d72b299a6ee
Mayank singh <115688857+mayanksingh-blip@users.noreply.github.com>
2024-09-29 13:56:45 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
+# This is to test the working of google genai
 import google.generativeai as genai
-api_key = "AIzaSyCCiUC5VY9wGtSLyoacU844awZbQ24bmTs"
+api_key = "your_api_key"
 
 # Set your API key (replace with your actual API key)
-genai.configure(api_key="AIzaSyCCiUC5VY9wGtSLyoacU844awZbQ24bmTs")
+genai.configure(api_key="your_api_key")
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Write a story about a magic backpack.")
